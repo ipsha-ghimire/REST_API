208eb3c8705324df675f8c03b6520ee1a04fab5a
Ipsha Ghimire <ipshagh2000@gmail.com>
2022-11-27 12:10:59 +0545

    get and post method done

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
 //creating a restful api with node express and mongodb 
 const express = require("express");
-const mongodb= require("mongodb");
-const db = require("./db");
+const {objectID,ObjectId}= require("mongodb");
+
+
 const {dbconnection,getDb}= require("./db")
 
 const app = express();
 
+app.use(express.json());
+let db;
 
-
-app.listen(3000,(req,res)=>{
-    console.log("app listening on port 3000")
+dbconnection((err)=>{
+  if(!err){
+    app.listen(3000,(req,res)=>{
+      console.log("app listening on port 3000")
+  })
+  db= getDb();
+  }
 })
 
-//get request
+
+//get all books
 app.get("/books",(req,res)=>{
-    let books=[]
-  db.collection("books").find().forEach(book => {books.push(book)
-  });  
+  let books=[];
+  db.collection("books").find().forEach(book =>books.push(book)
+  ).then(()=>{res.status(200).json(books)}).catch(()=>{res.status(501).json({err:"could not get books"})});  
+});
+
+//get individual books
+app.get("/books/:id",(req,res)=>{
+  if(ObjectId.isValid(req.params.id)){
+    db.collection("books").findOne({_id:ObjectId(req.params.id)}).then((result)=>{
+      res.status(200).json(result);
+    }).catch((err)=>{
+      res.status(500).json(err);
+    })
+  }
+  else{
+    res.status(500).json({err:"invalid document id"})
+  }
 })
+
+//post books 
+
+app.post("/books",(req,res)=>{
+const body=req.body;
+db.collection("books").insertOne(body).then((result)=>{res.status(201).json({result})}).catch((err)=>{
+  res.status(500).json({err:"could not insert document"});
+})
+})
+
+//delete books 
+
+
 
 
 
